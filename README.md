574c56d65b2b4bd8ecf3aec78d207d65a6a86285
itsvineet99 <vineetdorikar06@gmail.com>
2024-07-07 12:15:48 +0530

    code modify to take channel username and playlist  as input instead of playlist id
    instead of copying playlist id from url now user only need to provide channel username and playlist name.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Duration-counter-of-YT-playlist
-Uses YT API to measure  duration of yt playlists
+
+This is python programme which uses youtube api to measure duration of playlist.
+it takes username and name of playlist as input and gives you total duration of playlist.
--- YT_API.py
+++ YT_API.py
@@ -1,21 +1,46 @@
 from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
 import re
 from datetime import timedelta
-import os
-from dotenv import load_dotenv
-
 
 
 load_dotenv()
 api_key = os.getenv("api_key")
 
-youtube = build("youtube", "v3",developerKey=api_key)
+youtube = build("youtube", "v3", developerKey=api_key)
+
+user_name = input("Enter username of channel: ") 
+request_channel = youtube.channels().list(
+    part="statistics",
+    forUsername=user_name
+)
+
+response_channel = request_channel.execute()
+channel_id = response_channel["items"][0]["id"] 
+
+
+request_playlist = youtube.playlists().list(
+    part="snippet, contentDetails",
+    channelId=channel_id,
+    maxResults=50
+)
+
+response_playlist = request_playlist.execute()
+
+title = input("Enter the title of playlist: ")
+for playlist in response_playlist['items']:
+        if playlist['snippet']['title'] == title:   
+            print(f"Title: {playlist['snippet']['title']}")
+
+
+playlist_id = playlist["id"]
+
 
 hour_pattern = re.compile(r"(\d+)H")
 minute_pattern = re.compile(r"(\d+)M")
 second_pattern = re.compile(r"(\d+)S")
 
-playlist_id = input("Enter the id of playlist: ")
 
 total_seconds = 0
 
@@ -68,7 +93,4 @@ total_seconds = int(total_seconds)
 minutes, seconds = divmod(total_seconds, 60)
 hours, minutes = divmod(minutes, 60)
 
-print(f"{hours}H:{minutes}M:{seconds}S")
-
-
-
+print(f"Duration: {hours}H:{minutes}M:{seconds}S")
